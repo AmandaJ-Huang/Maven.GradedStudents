33435cf140aae7b860c7fbcdf9b990f3124de8bb
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-21 13:56:33 -0500

    student-features: addExamScore, setExamScore, getAvgScore

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -35,11 +35,29 @@ public class Student {
         for (int i = 0; i < examScores.size(); i++) {
             output += "\nExam " + (i+1) + " -> " + examScores.get(i);
         }
-        return "Exam Scores:\n" + output;
+        return "Exam Scores:" + output;
     }
 
     public Integer getNumberOfExamsTaken() {
         return this.examScores.size();
     }
 
+    public void addExamScore(double examScore) {
+        this.examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, double newScore) {
+        examScores.set(examNumber-1, newScore);
+    }
+
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        Integer size = examScores.size();
+
+        for (Double score : examScores) {
+            sum += score;
+        }
+
+        return sum/size;
+    }
 }
