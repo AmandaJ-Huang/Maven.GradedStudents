90bbb067f0ffed9443e753a5e7c27bf87d4b1b63
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-21 13:33:07 -0500

    defined instance vars, constructor, getters and setters

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,9 +1,45 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
     private String firstName;
     private String lastName;
     private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(testScores));
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getExamScores() {
+        String output = "";
+        for (int i = 0; i < examScores.size(); i++) {
+            output += "\nExam " + (i+1) + " -> " + examScores.get(i);
+        }
+        return "Exam Scores:\n" + output;
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return this.examScores.size();
+    }
+
 }
